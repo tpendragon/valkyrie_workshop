b43b7537c7e3b949de6e9a99a457b832c6f6f927
Trey Terrell <tterrell@ltopmacterrell.local>
2017-11-04 19:04:07 -0600

    Necessary files to spin up Vagrant.

--- app/models/postcard.rb
+++ app/models/postcard.rb
@@ -1,5 +1,9 @@
 class Postcard < Valkyrie::Resource
+  ## Every Resource which will be persisted needs an ID
   attribute :id, Valkyrie::Types::ID.optional
   attribute :title, Valkyrie::Types::Set
   attribute :author, Valkyrie::Types::Set
+  ## Member IDs is a special property. It's ordered, and has special queries.
+  ## The name is important.
+  attribute :member_ids, Valkyrie::Types::Array
 end
